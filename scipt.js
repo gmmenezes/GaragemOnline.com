14a3f6361a7f0047f69a27880ef2f99e04e52960
gmmenezes <153694958+gmmenezes@users.noreply.github.com>
2023-12-12 23:47:17 -0300

    SiteWeb1

---
+++ scipt.js
@@ -0,0 +1,14 @@
+
+const botao_p = document.querySelector(botao_p);
+botao_p.addEventListener('click', function(e){
+    e.preventDefault();
+    const botao_p = document.querySelector(Cadastrar);
+}
+)
+
+const botao_v = document.querySelector(botao_v);
+botao_v.addEventListener('click', function(e){
+    e.preventDefault();
+    const botao_v = document.querySelector(Cadastrar);
+}
+)
